1a08efb9d482bd4e3889ebec4470d89b99490a09
David Cruz Gómez <139822584+DavidCruzGomez@users.noreply.github.com>
2025-03-22 21:48:05 +0100

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -391,10 +391,11 @@ BEGIN
 	END TRY
 	BEGIN CATCH
 		PRINT '=========================================='
-		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
-		PRINT 'Error Message' + ERROR_MESSAGE();
-		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
-		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT 'Error Message: ' + ERROR_MESSAGE();
+		PRINT 'Error Number: ' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error State: ' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT 'Error Severity: ' + CAST (ERROR_SEVERITY() AS NVARCHAR);
+		PRINT 'Error Line: ' + CAST (ERROR_LINE() AS NVARCHAR);
 		PRINT '=========================================='
 	END CATCH
 END
