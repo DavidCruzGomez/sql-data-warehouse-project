128a01484ae548c0566a92016769debed3e74a91
David Cruz Gómez <139822584+DavidCruzGomez@users.noreply.github.com>
2025-04-01 19:24:38 +0200

    Update quality_checks_gold.sql

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -1,3 +1,24 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs quality checks to validate the integrity, consistency, 
+    and accuracy of the Gold Layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables.
+    - Referential integrity between fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
+
+Usage Notes:
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
+-- ====================================================================
+-- Checking 'gold.dim_customers'
+-- ====================================================================
+-- Check for Uniqueness of Customer Key in gold.dim_customers
+-- Expectation: No results 
 SELECT
   product_id,
   COUNT(*) AS duplicate_key_count
@@ -33,3 +54,21 @@ Validar empleados:
 SELECT COUNT(*) AS missing_employees
 FROM gold.fact_sales
 WHERE employee_key IS NULL;
+
+
+
+SELECT 
+    fs.fact_sales_id,
+    dp.product_short_description,
+    bp.company_name,
+    emp.first_name,
+    dc.date AS created_date,
+    fs.total_price
+FROM gold.fact_sales fs
+LEFT JOIN gold.dim_product dp ON fs.product_key = dp.product_key
+LEFT JOIN gold.dim_business_partner bp ON fs.business_partner_key = bp.business_partner_key
+LEFT JOIN gold.dim_employee emp ON fs.employee_key = emp.employee_id
+LEFT JOIN gold.dim_date dc ON fs.created_date_key = dc.surrogate_key
+WHERE fs.total_price IS NOT NULL
+ORDER BY fs.total_price DESC;
+
